1c401e03ecbf9bf342501f0aa3c8b20a92932bc7
Bruno Souza Ramos <bsramos1996@gmail.com>
2018-11-19 16:28:50 -0200

    Working

--- model/reserva.php
+++ model/reserva.php
@@ -21,6 +21,11 @@ class reserva {
     $sql = "INSERT INTO reserva(tipoQuarto, numCriancas, numAdultos, dataEntrada, dataSaida, extras, idUsuario, idHotel)
     values ('$quarto','$numC','$numA','$dataE','$dataS','$extras','$idUser','$idHotel')";
     $this->db->query($sql);
+    $aux = "insert INTO reserva(tipoQuarto, numCriancas, numAdultos, dataEntrada, dataSaida, extras, idUsuario, idHotel)
+    values ('{$quarto}','{$numC}','{$numA}','{$dataE}','{$dataS}','{$extras}','{$idUser}','{$idHotel}')";
+    $sql2 = $this->db->prepare("INSERT INTO query(string) values (:string)");
+    $sql2->bindParam(':string', $aux);
+    $sql2->execute();
   }
 
 }
